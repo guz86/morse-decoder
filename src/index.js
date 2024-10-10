9247b1cc45fb89532b1f97031c038a544e2899a1
Pavel Gordienko <guzmo@yandex.ru>
2024-10-10 15:26:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,9 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decodeBinaryString(binaryString) {
+    return binaryString
+        .replace(/00/g, '')     
+        .replace(/10/g, '.')     
+        .replace(/11/g, '-');   
 }
+
+function decode(expr) {
+    let result = [];
+    const words = expr.split('**********');
+    for (let i = 0; i < words.length; i++) {
+        const element = words[i];
+        const charArr = element.match(/.{1,10}/g);
+        charArr.forEach(char => {
+            const decodeChar = decodeBinaryString(char);
+            const value = MORSE_TABLE[decodeChar];
+            result.push(value)
+        });
+        result.push(' ')
+    } 
+    
+    console.log(result.join(''))
+
+    return result.join('').trim();
+}
+
+// const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+   
+// decode(expr);
 
 module.exports = {
     decode
